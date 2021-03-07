36288fa5edfe210cc5046754ba594ae5200722a9
Matchstix <matt.mui15@gmail.com>
2021-03-07 13:52:43 -0500

    pushing security updates

--- instagram_backend/models/user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var user = sequelize.define('user', {
-  }, {});
-  user.associate = function(models) {
-    // associations can be defined here
-  };
-  return user;
-};
